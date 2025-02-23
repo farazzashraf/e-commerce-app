bbb5a96c03e58c43abd0858e21927e42a6b56e9e
Faraz Ashraf <95475133+farazzashraf@users.noreply.github.com>
2025-02-23 16:21:45 +0530

    Delete firebase directory

--- firebase/firebase_auth.py
+++
@@ -1,8 +0,0 @@
-from firebase_admin import auth
-
-def verify_firebase_token(token):
-    try:
-        decoded_token = auth.verify_id_token(token)
-        return decoded_token  # This contains user info
-    except Exception:
-        return None
